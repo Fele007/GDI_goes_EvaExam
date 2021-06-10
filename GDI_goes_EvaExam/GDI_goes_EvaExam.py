cc88d197fb85f6ce5d118be04841528efb2efbf0
Fele007 <50261759+Fele007@users.noreply.github.com>
2021-06-10 19:31:17 +0200

    Added support for difficulty, not fully compatible with groups yet

--- GDI_goes_EvaExam/GDI_goes_EvaExam.py
+++ GDI_goes_EvaExam/GDI_goes_EvaExam.py
@@ -17,8 +17,12 @@ lists = re.compile('(</?ul>)|(</?li>)')
 if lists.search(html_string):
     print("Found list items or list templates (Formatvorlagen)! Clear your input, please!")
     exit()
-
+############ Fix known formatting errors that are safely identifiable
+html_string = re.compile(r'<a id.*?>.*?</a>').sub('', html_string)
 ############# HTML-String RegEx
+def rreplace(s, old, new, occurrence=-1):
+ li = s.rsplit(old, occurrence)
+ return new.join(li)
 # Handle paragraphs
 paragraphs = re.compile('<p>(.*?)</p>')
 html_string = paragraphs.sub(r'\1</br>', html_string)
@@ -31,20 +35,23 @@ html_string = groups.sub(r'\n\n\n\1', html_string)
 html_string = re.compile(r'((<[\w/]*>)|\s+)*SC(<[\w/]*>)*\s*</br>').sub(r'\n\nSC\n', html_string)
 # Find Questions
 tasks = re.compile(r'\n\nSC\n').split(html_string)
-html_string = ""
-for task in tasks:
+html_string = tasks[0]
+for task in tasks[1:]:
+    difficulty = 0
     # Find answers
     answer = re.compile(r'(?:<[\w/]*?>)*(\d\t)')
     if answer.findall(task):
+        # Extract difficulty
+        for digit_tab in answer.findall(task):
+            difficulty = max(difficulty, int(digit_tab[0]))
         # Seperate answers with new line
         task = answer.sub(r'\n\1', task)
         # Split question and answers to substitute tabs with html
         question_and_answer = answer.split(task, 1)
-        task = question_and_answer[0].replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;') + question_and_answer[1] + question_and_answer[2]
-    html_string += task + '\n\nSC\n'
+        task = question_and_answer[0].replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;') + question_and_answer[1] + question_and_answer[2].replace('</br>', '\n')
+    html_string += task + '\nDifficulty: ' + str(difficulty) + '\n\nSC\n'
 # Remove unnecessary SC at the end
 html_string = rreplace(html_string, '\n\nSC\n', '', 1)
-
 ############# Write document
 with open('output.txt', 'w') as html_file:
         html_file.write(html_string)
